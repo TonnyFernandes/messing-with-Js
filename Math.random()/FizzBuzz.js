74c8b99941b7a4ec2385bc261e82e3f0e245ee37
Tonny Fernandes <tonny.feh@gmail.com>
2025-03-07 17:43:37 -0300

    Update FizzBuzz.js

--- Math.random()/FizzBuzz.js
+++ Math.random()/FizzBuzz.js
@@ -1,7 +1,11 @@
 function FizzBuzz(input){
-    if(input%3!==0 && input%5!==0) return input;
     if(input%3===0 && input%5===0) return 'FizzBuzz';
     if(input%3===0) return 'Fizz';
-    return 'Buzz';
+    if(input%5===0) return 'Buzz'
+    return input;
 }
-console.log(FizzBuzz(Math.floor(Math.random()*100)));
+console.log("Random generation:");
+console.log(FizzBuzz(Math.ceil(Math.random() * 100)));
+
+console.log("\nNow, the 1-100 verification:");
+for(let i=1; i<=100; i++) console.log(FizzBuzz(i));
