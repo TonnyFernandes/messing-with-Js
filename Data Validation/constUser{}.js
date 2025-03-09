ba6676f841306982f36782132317ef1dcaa81206
Tonny Fernandes <tonny.feh@gmail.com>
2025-03-08 21:57:07 -0300

    Update constUser{}.js

--- Data Validation/constUser{}.js
+++ Data Validation/constUser{}.js
@@ -1,22 +1,46 @@
+const user = {
+    username: "",
+    email: "",
+    password: "",
+    confirmpassword: "",
+    age: "",
+};
+const alphanumeric = new Set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']);
+
 function validateUsername(){
-    let copy=user.username, c;
-    
     // Check if username have at least 5 of length
-    if(user.username.length < 5) return false;
+    if(user.username.length < 5) {
+        console.log("Username must have at least 5 of length\n");   
+        return false;
+    }
     
-    for(let i=user.username.length-1; i<=0; i--){
-        c = copy[i];
-        console.log(c);
+    // Check if the characters are all alphanumeric
+    for(let i=user.username.length-1; i>=0; i--){
+        let c = user.username[i];
+        if(!alphanumeric.has(c.toLowerCase())){
+            console.log('Only alphanumeric characters will be accepted. No spaces or special characters\n');
+            return false;
+        }
     }
     
     return true;
 }
-function registration(){
+function validadeEmail(){
+    return true;
+}
+function registration(user){
     // Read and validate Username
     do{
         user.username = prompt("Your username: ");
-        if(!validateUsername(user.username)){
-            console.log("Invalid username\n");
-        }
     }while(!validateUsername(user));
+    
+    // Read and validate Email
+    do{
+        user.email = prompt("")
+    }
 }
+
+// Main functionality
+console.log('Enter all the asked data');
+registration(user);
+console.log(user);
