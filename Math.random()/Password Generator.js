21298ea7e41fdf3ee553db8aec0f3e1b7d516d14
Tonny Fernandes <tonny.feh@gmail.com>
2025-03-09 15:16:44 -0300

    Update Password Generator.js

--- Math.random()/Password Generator.js
+++ Math.random()/Password Generator.js
@@ -1,30 +1,72 @@
 const LowerCaseAlphabet = new Set('abcdefghijklmnopqrstuvwxyz');
 const UpperCaseAlphabet = new Set('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-const numbers = new Set('0123456789');
-const symbols = new Set(`!@#$%&*+=-_.,;:?/|\\{}[]()<>`);
+const Numbers = new Set('0123456789');
+const Symbols = new Set(`!@#$%&*+=-_.,;:?/|\\{}[]()<>`);
 
+const lower = Array.from(LowerCaseAlphabet);
+const upper = Array.from(UpperCaseAlphabet);
+const numbers = Array.from(Numbers);
+const symbols = Array.from(Symbols);
+
+
+
+function instructions(){
+    console.log('This code generates passwords following this rules:')
+    console.log('\t- Have more than 8 characters');
+    console.log('\t- Have less than 16 characters');
+    console.log('\t- Have at least one lowercase letter');
+    console.log('\t- Have at least one uppercase letter');
+    console.log('\t- Have at least one special character');
+    console.log('\t\t. with them being !@#$%&*+=-_.,;:?/|\\{}[]()<>');
+}
+function askLength(){
+    let length;
+    do{
+        length = prompt('\nEnter the password length ');
+        length = Number(length);
+    }while(!validateLength(length));
+    return length;
+}
 function validateLength(length){
     if(isNaN(length)){
-        console.log('Length is Not a Number\n')
+        console.log('Length is Not a Number')
         return false;
     }
     
     if(length < 8 || length > 16){
-        console.log('Length must be between 8 and 16\n');
+        console.log('Length must be between 8 and 16');
         return false;
     }
     
     return true;
 }
-function askLength(){
-    let length;
-    do{
-        length = prompt('\nEnter the password length');
-        length = Number(length);
-    }while(!validateLength(length);
-    return length;
-}
 
+function selectChar(x){
+    switch(x){
+        case 0:
+            return lower[Math.floor(Math.random() * lower.length)]
+        case 1:
+            return upper[Math.floor(Math.random() * upper.length)]
+        case 2:
+            return numbers[Math.floor(Math.random() * numbers.length)];
+        case 3:
+            return symbols[Math.floor(Math.random() * symbols.length)];
+    }
+}
+function passwordGenerator(passwordLength){
+
+    do{
+       let i=0; 
+        do{
+            let c = selectChar(Math.floor(Math.random()*4));
+            password.push(c);
+            i++;
+        }while(i<passwordLength);
+        
+    }while(!validatePassword(password));
+    
+    return password;
+}
 function validatePassword(password){
     
     // Check password length
@@ -44,8 +86,8 @@ function validatePassword(password){
         // Haha if go brr
         if(LowerCaseAlphabet.has(c)) hasLower = true;
         if(UpperCaseAlphabet.has(c)) hasUpper = true;
-        if(numbers.has(c)) hasNumbers = true;
-        if(symbols.has(c)) hasSymbols = true;
+        if(Numbers.has(c)) hasNumbers = true;
+        if(Symbols.has(c)) hasSymbols = true;
     }
     
     // Second and last step at checking password
@@ -57,22 +99,14 @@ function validatePassword(password){
     return true;
 }
 
-function passwordGenerator(password){
-    let SetSelector = Math.floor(Math.random() * 4)
-    do{
-        SetSelector = Math.floor(Math.random() * 4);
-    }while(!validatePassword(password));
-}
 
-console.log('This code generates passwords following this rules:')
-console.log('\t- Have more than 8 characters');
-console.log('\t- Have less than 16 characters');
-console.log('\t- Have at least one lowercase letter');
-console.log('\t- Have at least one uppercase letter');
-console.log('\t- Have at least one special character');
-console.log('\t\t. with them being !@#$%&*+=-_.,;:?/|\\{}[]()<>');
-
-
+// Main code flow
+instructions();
 const passwordLength = askLength();
-let password = "";
-passwordGenerator(password);
+let password = []; 
+password = passwordGenerator(passwordLength);
+let p = '';
+for(character of password){
+    p+=character;
+}
+console.log(`Your password: ${p}`);
